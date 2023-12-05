0aeb83eb3886b2eb2a8720a27ea8ad7e7d5db0fd
Albin-Eldhose <albineldhose37@gmail.com>
2023-12-05 10:20:15 +0530

    c

---
+++ lib/app/modules/bottom_navigation/views/bottom_navigation_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/bottom_navigation_controller.dart';
+
+class BottomNavigationView extends GetView<BottomNavigationController> {
+  const BottomNavigationView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
