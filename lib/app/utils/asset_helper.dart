553b7ea6cf7c915c2673d5ca2bd18f5358c03304
shijo2000 <shijo9863@gmail.com>
2023-12-16 15:34:15 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/app/modules/add_test/controllers/add_test_controller.dart
    #	lib/app/modules/payment/controllers/payment_controller.dart

--- lib/app/utils/asset_helper.dart
+++ lib/app/utils/asset_helper.dart
@@ -4,16 +4,16 @@ abstract class AssetHelper {
   }
 
   //==========================[images]=======================
-  static String laboratoryLogoSelected = images('laboratoryLogoSelected.png');
+  static String laboratoryLogoSelected = images('laboratory(select).png');
   static String houseLogo = images('allocatedLogos.png');
-  static String deliveryLogo = images('deliverylogoUnselected.png');
-  static String historyLogo = images('historylogoUnselected.png');
-  static String logoutLogo = images('logoutlogoUnselected.png');
+  static String deliveryLogo = images('delivery.png');
+  static String historyLogo = images('history.png');
+  static String logoutLogo = images('logout 2.png');
   static String macareLogo = images('Macare-logo(R) (1).png');
-  static String laboratoryLogo = images('microscopeLogoUnselected.png');
-  static String deliveryLogoSelected = images('deliveryLogoSelected.png');
-  static String historyLogoSelected = images('historyLogoSelected.png');
-  static String logoutLogoSelected= images('logoutLogoSelected.png');
+  static String laboratoryLogo = images('laboratory.png');
+  static String deliveryLogoSelected = images('delivery (select).png');
+  static String historyLogoSelected = images('history(select).png');
+  static String logoutLogoSelected= images('logout(select).png');
 
 
 
